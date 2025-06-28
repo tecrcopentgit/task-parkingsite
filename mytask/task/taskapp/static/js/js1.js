1df3274ebb9023f621cddce3c5aa3bf6a537ad01
Abdul Haadhi <abdulhaadhi015@gmail.com>
2025-06-28 22:14:10 +0530

    Delete mytask/task directory

--- mytask/task/taskapp/static/js/js1.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(element) {
-    const answer = element.nextElementSibling;
-    const isVisible = answer.style.display === "block";
-    
-    
-    document.querySelectorAll('.faq-answer').forEach(ans => ans.style.display = "none");
-    answer.style.display = isVisible ? "none" : "block";
-  }
